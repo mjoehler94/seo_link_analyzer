3709df77d495a1be3bfbed5ce2ba6d25a8381621
mjoehler94 <mjoehler94@gmail.com>
2022-06-09 21:08:08 -0600

    fix bugs

--- crawl.py
+++ crawl.py
@@ -3,6 +3,10 @@ from lxml import html
 
 def get_urls_from_string(page_content: str, base_url: str) -> list:
     # note that base url can be added to make links absolute instead of from string
-    tree = html.fromstring(page_content, base_url).make_links_absolute()
-    links = [elem.get("href") for elem in tree.iter() if elem.tag() == "a"]
+    tree = html.fromstring(page_content, base_url)
+    tree.make_links_absolute()
+    links = [elem.get("href") for elem in tree.iter() if elem.tag == "a"]
     return links
+
+def normalize_url(url: str) -> str:
+    return
