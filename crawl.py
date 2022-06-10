0796d446297145094cd740a192dbb77f0bc70615
mjoehler94 <mjoehler94@gmail.com>
2022-06-09 21:01:34 -0600

    finish step 4

--- crawl.py
+++ crawl.py
@@ -1,6 +1,8 @@
 from lxml import html
 
 
-def get_url_from_string(page_content: str, base_url: str) -> list:
-
-    return
+def get_urls_from_string(page_content: str, base_url: str) -> list:
+    # note that base url can be added to make links absolute instead of from string
+    tree = html.fromstring(page_content, base_url).make_links_absolute()
+    links = [elem.get("href") for elem in tree.iter() if elem.tag() == "a"]
+    return links
--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import unittest
 
 from report import remove_none_values, sort_pages
-from crawl import get_url_from_string
+from crawl import get_urls_from_string
 
 
 class Tests(unittest.TestCase):
@@ -13,12 +13,12 @@ class Tests(unittest.TestCase):
 
     def test_sort_pages(self):
         self.assertEqual(
-            sort_pages({"first": 45, "third": 15, "second": 25}),
             [("first", 45), ("second", 25), ("third", 15)],
+            sort_pages({"first": 45, "third": 15, "second": 25}),
         )
         self.assertEqual(
-            sort_pages({"first": 45, "third": 46, "second": 47}),
             [("second", 47), ("third", 46), ("first", 45)],
+            sort_pages({"first": 45, "third": 46, "second": 47}),
         )
 
     def test_get_url_from_string(self):
@@ -29,7 +29,23 @@ class Tests(unittest.TestCase):
                 </body>
             </html>
         """
-        self.assertEqual()
+        base_url_example = ""
+
+        def test_get_urls_from_string(self):
+            self.assertEqual(
+                ["https://blog.boot.dev"],
+                get_urls_from_string(
+                    '<html><body><a href="https://blog.boot.dev"><span>Boot.dev></span></a></body></html>',
+                    "https://blog.boot.dev",
+                ),
+            )
+            self.assertEqual(
+                ["https://blog.boot.dev", "https://wagslane.dev"],
+                get_urls_from_string(
+                    '<html><body><a href="https://blog.boot.dev"><span>Boot.dev></span></a><a href="https://wagslane.dev"><span>Boot.dev></span></a></body></html>',
+                    "https://blog.boot.dev",
+                ),
+            )
 
 
 if __name__ == "__main__":
